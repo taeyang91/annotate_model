0d336540f8820631776232a3f51c51e1101edd4f
Taeyang Lee <taeyang91@gmail.com>
2025-01-15 12:42:21 -0700

    Add CLI for annotating models
    Usage:
    - To annotate all models: `annotate models -a`
    - To annotate a specific model: `annotate models [model_name]`
    - To display help message: `annotate models -h`

---
+++ lib/annotate_model/cli.rb
@@ -0,0 +1,29 @@
+module AnnotateModel
+  class CLI
+    def self.start(args)
+      options = {}
+      OptionParser.new do |opts|
+        opts.banner = "Usage: annotate models [options] [model_name]"
+
+        opts.on("-a", "--all", "Annotate all models") do |a|
+          options[:all] = a
+        end
+
+        opts.on("-h", "--help", "Prints this help") do
+          puts opts
+          exit
+        end
+      end.parse!(args)
+
+      if options[:all]
+        AnnotateModel::Annotator.annotate_all
+      elsif args.any?
+        model_name = args.first
+        AnnotateModel::Annotator.annotate_single(model_name)
+      else
+        puts "Please specify a model name or use -a for all models."
+        exit(1)
+      end
+    end
+  end
+end
